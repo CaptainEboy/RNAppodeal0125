8509b607b5fc4bd52e88dd0428f4258ddb9fd9e5
CaptainEboy <emaapps.ac24@gmail.com>
2025-01-17 16:12:43 +0100

    App crashing

--- App.js
+++ App.js
@@ -1,7 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {
+  Appodeal,
+  AppodealAdType
+} from 'react-native-appodeal';
 
-export default function App() {
+const adTypes = AppodealAdType.INTERSTITIAL | AppodealAdType.REWARDED_VIDEO | AppodealAdType.BANNER;
+Appodeal.initialize('a5113f732b3f6a6e7a74d38ab11b4c5edcde3f856ea17555', adTypes)
+
+
+
+
+export default function App() { 
+  Appodeal.addEventListener(AppodealBannerEvent.SHOWN, () =>
+      console.log("Banner shown")
+  );
+  Appodeal.addEventListener(AppodealBannerEvent.EXPIRED, () =>
+      console.log("Banner expired")
+  );
+  Appodeal.addEventListener(AppodealBannerEvent.CLICKED, () =>
+      console.log("Banner was clicked")
+  );
+  Appodeal.addEventListener(AppodealBannerEvent.FAILED_TO_LOAD, () =>
+      console.log("Banner failed to load")
+  );
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
