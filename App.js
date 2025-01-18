8c76bc446129802a22b9749e8b1adfd7f7e2625e
CaptainEboy <emaapps.ac24@gmail.com>
2025-01-18 12:44:49 +0100

    fixed eror, added interstitial and banner ads, test mode

--- App.js
+++ App.js
@@ -12,18 +12,24 @@ Appodeal.initialize('a5113f732b3f6a6e7a74d38ab11b4c5edcde3f856ea17555', adTypes)
 
 
 export default function App() { 
-  Appodeal.addEventListener(AppodealBannerEvent.SHOWN, () =>
-      console.log("Banner shown")
-  );
-  Appodeal.addEventListener(AppodealBannerEvent.EXPIRED, () =>
-      console.log("Banner expired")
-  );
-  Appodeal.addEventListener(AppodealBannerEvent.CLICKED, () =>
-      console.log("Banner was clicked")
-  );
-  Appodeal.addEventListener(AppodealBannerEvent.FAILED_TO_LOAD, () =>
-      console.log("Banner failed to load")
-  );
+  // Appodeal.addEventListener(AppodealBannerEvent.SHOWN, () =>
+  //     console.log("Banner shown")
+  // );
+  // Appodeal.addEventListener(AppodealBannerEvent.EXPIRED, () =>
+  //     console.log("Banner expired")
+  // );
+  // Appodeal.addEventListener(AppodealBannerEvent.CLICKED, () =>
+  //     console.log("Banner was clicked")
+  // );
+  // Appodeal.addEventListener(AppodealBannerEvent.FAILED_TO_LOAD, () =>
+  //     console.log("Banner failed to load")
+  // );
+
+  // Show interstitial for specific pacement
+Appodeal.show(AppodealAdType.INTERSTITIAL)
+
+//Show banner at the top of screen
+Appodeal.show(AppodealAdType.BANNER_BOTTOM)
 
   return (
     <View style={styles.container}>
